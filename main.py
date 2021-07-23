52cd1ec1b36509505ca61080fed7271b722fb56c
ivan-desktop <kovachivanagustin@gmail.com>
2021-07-23 12:18:59 -0300

    Base de datos creada

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import re
 import pandas as pd
 pd.options.mode.chained_assignment = None
 
+# Conexión a la base de datos
+
+conn = sqlite3.connect("db_autos")
+cur = conn.cursor()
+cur.executescript("""
+    DELETE FROM Cliente;
+    DELETE FROM Auto;
+    DELETE FROM Auto_Cliente;
+    DELETE FROM Reparacion;
+""")
+conn.commit()
+
 # Abrir el csv y eliminar una columna bug
 df = pd.read_csv("autos.csv", usecols=["Cliente", "Auto", "Trabajo", "Kilometraje", "Fecha", "Patente"])
 
 # Dar formato a la fecha
-
 df.loc[df["Fecha"].str.contains("\?|Km", na=False).values, "Fecha"] = "17/1/2019"
 df["Fecha"] = pd.to_datetime(df["Fecha"], dayfirst=True)
-print(df["Fecha"])
 
 # Dar formato al kilometraje
 df.loc[pd.isnull(df["Kilometraje"]), "Kilometraje"] = "0" # Transforma los valores nulos o vacíos en el csv, a "0"
@@ -33,11 +43,6 @@ df.loc[pd.isnull(df["Trabajo"]), "Trabajo"] = "Desconocido"
 # Dar formato a la patente
 df.loc[pd.isnull(df["Patente"]), "Patente"] = "Desconocida"
 
-print(df.info())
 
-# Conexión a la base de datos
-# conn = sqlite3.connect("autos")
-# cur = conn.cursor()
-# cur.execute("DELETE FROM clientes WHERE true")
-# conn.commit()
-# cur.close()
+cur.close()
+
