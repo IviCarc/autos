381dfb9fbbeb547eed6fb36ca2cd65999958c85d
ivan <kovachivanagustin@gmail.com>
2022-01-11 22:53:41 -0300

    lista de clientes, lista de clientes por regex, busqueda por patente y primera linea de bashscript

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+
 import sqlite3
 import pandas as pd
 import sys
+import re
 
 conn = sqlite3.connect("/home/ivan/Documents/programacion/autos/db_autos")
 cur = conn.cursor()
@@ -49,11 +52,96 @@ def getByClient(client):
             fila = f'{client} | {modelo} | {trabajo} | {km} Km | {fecha} | {patente}'
             print(fila)
 
+
+def listClients():
+    cur.execute("""
+        SELECT *
+        FROM Cliente
+    """)
+    clients = cur.fetchall()
+    for client in clients: print(f'{client[0]}, {client[1]}')
+
+def regexp(expr, item):
+    reg = re.compile(expr)
+    return reg.search(item) is not None
+
+conn.create_function("REGEXP", 2, regexp)
+
+def searchClients(regex):
+    
+    cur.execute("""
+        SELECT *
+        FROM Cliente
+        WHERE cliente REGEXP (?)
+    """, (regex, ))
+    clients = cur.fetchall()
+    if not clients: print('No matches')
+    else:
+        for client in clients: print(f'{client[0]}, {client[1]}')
+
+
+def getByPatent(patente):
+    patente = patente.upper()
+    cur.execute("""
+        SELECT *
+        FROM Auto_Cliente
+        WHERE patente = (?)
+    """, (patente, ))
+    auto = cur.fetchall()[0]
+
+    auto_cliente_id = auto[0]
+    client_id = auto[1]
+    auto_id = auto[2]
+
+    cur.execute("""
+        SELECT cliente
+        FROM Cliente
+        WHERE id = (?);
+    """, (client_id, ))
+
+    cliente = cur.fetchone()[0]
+
+    cur.execute("""
+        SELECT modelo
+        FROM Auto
+        WHERE id = (?)
+    """, (auto_id, ))
+
+    modelo = cur.fetchone()[0]
+
+    cur.execute("""
+        SELECT fecha, kilometraje, trabajo
+        FROM Reparacion
+        WHERE auto_cliente_id = (?)
+    """, (auto_cliente_id, ))
+
+    reparaciones = cur.fetchall()
+
+    for reparacion in reparaciones:
+        fecha = reparacion[0]
+        km = reparacion[1]
+        trabajo = reparacion[2]
+
+        fila = f'{cliente} | {modelo} | {trabajo} | {km}Km | {fecha} | {patente}'
+
+        print(fila)
+
+    # print(auto)
+    # print(cliente)
+    # print(modelo)
+    # print(fecha, km,trabajo)
+
+
 args = sys.argv
-print(args)
 
 for i in range(len(args)):
     if args[i] == args[0]:
         continue
     if args[i] == '--client':
         getByClient(args[i + 1])
+    if args[i] == '-l':
+        listClients()
+    if args[i] == '--search':
+        searchClients(args[i + 1])
+    if args[i] == '--patent':
+        getByPatent(args[i + 1])
